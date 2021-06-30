f98a99d6aedf4c58b5a975253c7cb586c936df3e
Eduardo Cruz <edufracruz@gmail.com>
2021-06-30 09:53:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function verificar() {
     let pan = parseFloat(window.prompt(`Qual era o preço anterior do produto?`).replace(',', '.'))
-    let pat = parseFloat(window.prompt(`Qual é o preço antual do produto?`).replace(',', '.'))
+    let pat = parseFloat(window.prompt(`Qual é o preço atual do produto?`).replace(',', '.'))
     let dif1 = (pat - pan)
     let por1 = (dif1 / pan) * 100
     let dif2 = (pan - pat)
@@ -17,5 +17,8 @@ function verificar() {
         res.innerHTML += '<p>Hoje o produto está mais barato.</p>'
         res.innerHTML += `<p>O preço caiu ${dif2.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} em relação ao preço anterior.</p>`
         res.innerHTML += `<p>Uma variação de ${por2.toLocaleString('pt-br',{maximumFractionDigits: 2})}% para baixo.</p>`
+    } else {
+        window.alert('Por favor digite um valor válido')
+        res.innerHTML = '<p>Clique no botão acima para analisar a mudança de preço</p>'
     }
 }
